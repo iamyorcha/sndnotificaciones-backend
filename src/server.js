133d5c2e4b8c4da586ff8a812d1094b583e8b6ef
iamyorcha <119068667+iamyorcha@users.noreply.github.com>
2025-10-03 11:44:37 -0600

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const routes = require('./routes');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+
+app.use('/api', routes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor de sndnotificaciones corriendo en puerto ${PORT}`);
+});
